0f811b81b239ce013f7d2e37221455ac3a15ce68
Chiranjit Karmakar <62233288+chiranjit2020@users.noreply.github.com>
2021-12-23 12:18:26 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -26,3 +26,25 @@ function openNav(){
 }
 
 openNav();
+
+//Footer Year
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+//Form Submission
+
+jQuery('#frmSubmit').on('submit',function(e){
+	e.preventDefault();
+	jQuery('#msg').html('Please wait...').show();
+	jQuery('#btnSubmit').attr('disabled',true);
+	jQuery.ajax({
+		url:'https://script.google.com/macros/s/AKfycbzdNnii7jA9vGnU4VfnSPVmsSWP7oKuVGIQeWFwe2zoot3DhzeitA8-OTjmTYG3wZku3g/exec',
+		type:'post',
+		data:jQuery('#frmSubmit').serialize(),
+		success:function(result){
+			jQuery('#frmSubmit')[0].reset();
+			jQuery('#msg').html('Thank You For Con');
+			jQuery('#btnSubmit').attr('disabled',false);
+			//window.location.href='';
+		}
+	});
+});
