343cee4987f164d409bd32713e99dd0dac8f1e92
Chiranjit Karmakar <62233288+chiranjit2020@users.noreply.github.com>
2021-12-25 20:58:13 +0530

    Add files via upload
    Responsive started. Form Fixed.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
-//Upor(Back to top button) Script
-const button = document.querySelector("#upor");
-// UPOR(Element, Speed, Offset)
-UPOR(button,500,100);
+const button = document.querySelector("#back2top");
+// Call the BACK2TOP Function
+// BACK2TOP(element, offsetToValue)
+BACK2TOP(button, 200);
 
 
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 //Mobile Menu
 
 function closeNav(){
@@ -30,21 +35,46 @@ openNav();
 //Footer Year
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
-//Form Submission
+const form = document.getElementById('form');
+const result = document.getElementById('msg');
 
-jQuery('#frmSubmit').on('submit',function(e){
-	e.preventDefault();
-	jQuery('#msg').html('Please wait...').show();
-	jQuery('#btnSubmit').attr('disabled',true);
-	jQuery.ajax({
-		url:'https://script.google.com/macros/s/AKfycbzdNnii7jA9vGnU4VfnSPVmsSWP7oKuVGIQeWFwe2zoot3DhzeitA8-OTjmTYG3wZku3g/exec',
-		type:'post',
-		data:jQuery('#frmSubmit').serialize(),
-		success:function(result){
-			jQuery('#frmSubmit')[0].reset();
-			jQuery('#msg').html('Thank You For Con');
-			jQuery('#btnSubmit').attr('disabled',false);
-			//window.location.href='';
-		}
-	});
+form.addEventListener('submit', function(e) {
+    const formData = new FormData(form);
+    e.preventDefault();
+    var object = {};
+    formData.forEach((value, key) => {
+        object[key] = value
+    });
+    var json = JSON.stringify(object);
+    result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+            	result.style.display = "inline-block";
+                result.innerHTML = json.message;
+            } else {
+                console.log(response);
+                result.style.display = "inline-block";
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
 });
