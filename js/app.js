01428d56a82fae08edf1be139c5b08730cc4f16e
Chiranjit Karmakar <62233288+chiranjit2020@users.noreply.github.com>
2022-02-24 11:32:57 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 'use strict'
 
-window.addEventListener("load", function (event) {
-    let loaderImg = document.getElementById('preloader');
-    loaderImg.style.display = 'none';
+window.addEventListener('load', function () {
+    let preloader = document.createElement('div');
+    preloader.setAttribute('id', 'preloader');
+    preloader.innerHTML = `<img src="images/bird.gif" alt="loader" />`;
+    
+    if( document.body != null ){
+        document.body.prepend(preloader);
+    }
+    
+    setTimeout(removeLoader, 2000);
+    
+    function removeLoader(){
+        preloader.remove();
+    }
 });
 
 //Dark Mode -Light Mode
