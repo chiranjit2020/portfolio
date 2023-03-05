175668d6195a3a1386424c25f483cbc02fca30ff
Chiranjit Karmakar <62233288+chiranjit2020@users.noreply.github.com>
2023-03-06 02:51:13 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,59 @@
+//Loader
+window.onload = function () {
+    setTimeout(function () {
+        document.getElementById("fadein").remove();
+    }, 1000);
+};
+
+$(window).on('load', function () {
+    $("#loader-wrapper").fadeOut(700);
+});
+
+//Tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+//Form Submit
+const form = document.getElementById('form');
+const result = document.getElementById('msg');
+
+form.addEventListener('submit', function (e) {
+    const formData = new FormData(form);
+    e.preventDefault();
+    var object = {};
+    formData.forEach((value, key) => {
+        object[key] = value
+    });
+    var json = JSON.stringify(object);
+    result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.style.display = "inline-block";
+                result.innerHTML = json.message;
+            } else {
+                console.log(response);
+                result.style.display = "inline-block";
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function () {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
